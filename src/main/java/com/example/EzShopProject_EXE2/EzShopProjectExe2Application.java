c4a300d42b6e35054cb647cc0037644942941298
chanhtong123 <chanhtong11@gmail.com>
2024-05-08 17:35:04 +0700

    Create project

---
+++ src/main/java/com/example/EzShopProject_EXE2/EzShopProjectExe2Application.java
@@ -0,0 +1,13 @@
+package com.example.EzShopProject_EXE2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzShopProjectExe2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzShopProjectExe2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EzShopProject_EXE2/EzShopProjectExe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EzShopProject_EXE2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzShopProjectExe2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
