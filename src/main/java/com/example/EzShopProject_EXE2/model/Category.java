3b2326c126f71131af20e0bccea5fc880e9c6296
chanhtong123 <chanhtong11@gmail.com>
2024-05-09 22:01:45 +0700

    Category and Voucher

---
+++ src/main/java/com/example/EzShopProject_EXE2/model/Category.java
@@ -0,0 +1,19 @@
+package com.example.EzShopProject_EXE2.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "category")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
