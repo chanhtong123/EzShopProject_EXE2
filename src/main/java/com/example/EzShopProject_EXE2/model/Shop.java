2b821981db4bcdaa01f1b0c712a5b156b094af39
ngo13082k2 <89001718+ngo13082k2@users.noreply.github.com>
2024-05-09 22:18:39 +0700

    data

--- src/main/java/com/example/EzShopProject_EXE2/model/Shop.java
+++ src/main/java/com/example/EzShopProject_EXE2/model/Shop.java
@@ -14,10 +14,21 @@ public class Shop {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 private Long shopId;
+    @Column(name = "name", length = 500)
     private String name;
+
+    @Column(name = "address", length = 500)
     private String address;
+
+    @Column(name = "phone_number", length = 500)
     private String phoneNumber;
+
+    @Column(name = "wallet")
     private double wallet;
+
+    @Column(name = "status")
     private int status;
+
+    @Column(name = "owner")
     private Long owner;
 }
