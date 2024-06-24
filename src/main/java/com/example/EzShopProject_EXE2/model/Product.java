aab0fe6a1f4a1ba248626072aa68a34fbc5862b3
ngo13082k2 <89001718+ngo13082k2@users.noreply.github.com>
2024-06-25 02:32:39 +0700

    fix/product

--- src/main/java/com/example/EzShopProject_EXE2/model/Product.java
+++ src/main/java/com/example/EzShopProject_EXE2/model/Product.java
@@ -41,7 +41,7 @@ public class Product {
     private String image4;
 
     @Temporal(TemporalType.TIMESTAMP)
-    @Column(nullable = false, updatable = false)
+//    @Column(nullable = false, updatable = false)
     private Date createdAt = new Date();
 
 
